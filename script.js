f5819fce4d0161a352825f8fe27f6b1148ddf738
ressignelli <61521373+ressignelli@users.noreply.github.com>
2024-03-26 14:51:10 -0300

    projeto inicial

---
+++ script.js
@@ -0,0 +1,18 @@
+﻿function Excluir(){
+	alert("Excluido");
+}
+
+function Habilitar(){
+	const x = document.forms["form1"];
+	for (i=0; i<x.length; i++){
+		x.elements[i].disabled = false;
+	}
+}
+
+function Desabilitar(){
+	const x = document.forms["form1"];
+
+	for (i=0; i < x.length; i++){
+		x.elements[i].disabled = true;
+	}
+}
